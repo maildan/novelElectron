d0988ec65df7cc4136b2698386eaef295f80da6b
umsehun <sakills914@gmail.com>
2025-08-02 23:54:06 +0900

    feat:first commit

---
+++ src/renderer/.next/static/chunks/src_renderer_app_analytics_page_tsx_b7073428._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_12c5c352._.js"
+],
+    source: "dynamic"
+});
---
+++ src/renderer/.next/static/chunks/src_renderer_app_projects_page_tsx_b7073428._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_renderer_78e74d6e._.js",
+  "static/chunks/node_modules_lucide-react_dist_esm_icons_b91c8ee2._.js"
+],
+    source: "dynamic"
+});
